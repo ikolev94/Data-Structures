866e8ce3299d6fdb3bff98be61f69fa676b0a5ac
Ivan Kolev <ikolev94@yahoo.bg>
2016-04-25 19:44:55 +0300

    add list type string

--- Java/src/linkedListDemo/LinkedListTest.java
+++ Java/src/linkedListDemo/LinkedListTest.java
@@ -4,16 +4,28 @@ public class LinkedListTest {
 
 	public static void main(String[] args) {
 
-		LinkedList<Integer> myList = new LinkedList<>();
+		LinkedList<Integer> numbers = new LinkedList<>();
 
-		myList.add(1);
-		myList.add(2);
-		myList.add(3);
-		myList.add(4);
-		myList.remove(0);
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+		numbers.add(4);
+		numbers.remove(0);
 
-		for (Integer integer : myList) {
-			System.out.println(integer);
+		for (Integer number : numbers) {
+			System.out.println(number);
+		}
+
+		LinkedList<String> words = new LinkedList<>();
+
+		words.add("This");
+		words.add("Is");
+		words.add("Is");
+		words.add("Test");
+		words.remove(2);
+
+		for (String word : words) {
+			System.out.println(word);
 		}
 
 	}
